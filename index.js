5f7a9d5b008ac6b693af4ed1e006ad23bd87df8f
RenatoSouza <renatoserra@gmail.com>
2019-01-07 21:39:11 +0000

    inicio

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+*
+* Primary file for the api 
+*/
+
+// Dependencies
+const server =require('./lib/server');
+const works = require('./lib/workers');
+ 
+// Declare the app
+const app = {};
+
+// Init function
+app.init = function() {
+    // Start the server
+    server.init();
+
+    // Start the workers
+      works.init();
+ 
+};
+
+// Execute function
+app.init();
+
+// Export the app
+module.exports = app;
